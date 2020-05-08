ea97688a15a069c0d0932abe6573c5c452d81728
Yassine JOUT <yassinejout@gmail.com>
2020-05-08 17:15:18 +0000

    keepBuild

---
+++ build/precache-manifest.d2a744bf7ce1aaa8c15c213cd2bae62e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5247fb96f77a3717ca74b9cadf870460",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee81fdafc0fc085c90ec",
+    "url": "/static/js/2.d8db886d.chunk.js"
+  },
+  {
+    "revision": "bdfdaee5779fe0c6a048c3619e9bf199",
+    "url": "/static/js/2.d8db886d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fabf7785232a7deb1e15",
+    "url": "/static/js/main.f00a6881.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
