d3999c8f17f24f5670161ed4acfeabe12582001d
Yassine JOUT <yassinejout@gmail.com>
2020-06-13 20:22:37 +0100

    feat(ProfilePage): profile header

---
+++ src/components/Profile/ProfileHeader/index.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import {
+  Container,
+  Image,
+  Grid,
+  Icon,
+  Segment,
+  Placeholder,
+  Button,
+  Modal,
+  Header,
+} from "semantic-ui-react";
+import img from "../../../images/centered-paragraph.png";
+const ProfileHeader: React.FunctionComponent = () => {
+  const userHandlre = "@yassine";
+  const userBio = "Bio : A little something about me";
+  const userAddress = "Paris";
+  return (
+    <div>
+      <div className="profile-header-container">
+        <div
+          className="profile-header-cover"
+          style={{
+            backgroundImage: `url('./images/cover.png')`,
+          }}
+        >
+          <Grid columns={3} stackable>
+            <Grid.Column></Grid.Column>
+
+            <Grid.Column>
+              <Image
+                className="profile-photo"
+                src="./images/square-image.png"
+                size="small"
+                circular
+                centered
+              />
+              <Container>
+                <Grid>
+                  <Grid.Row centered>
+                    <div className="profile-btn-controls">
+                      <span className="profile-btn-controls-items">
+                        {userHandlre}
+                      </span>
+                      <br />
+                      <br />
+                      <span className="profile-btn-controls-items">
+                        {userBio}
+                      </span>
+                      <br />
+                      <br />
+                      <span className="profile-btn-controls-items">
+                        <Icon name="map marker alternate" color="grey" />
+                        {userAddress}
+                      </span>
+                    </div>
+                  </Grid.Row>
+                </Grid>
+              </Container>
+            </Grid.Column>
+
+            <Grid.Column>
+              <Container>
+                <Grid>
+                  <Grid.Row centered>
+                    <div className="profile-btn-controls-btns">
+                      <Modal
+                        trigger={
+                          <span className="profile-btn-controls-items" style={{cursor: 'pointer'}}>
+                            Edit Profile
+                          </span>
+                        }
+                      >
+                        <Modal.Header>Edit Profile</Modal.Header>
+                        <Modal.Content >
+                        
+                          <Modal.Description>
+                          
+                          </Modal.Description>
+                        </Modal.Content>
+                      </Modal>
+
+                      <br />
+                      <br />
+                      <span className="profile-btn-controls-items" style={{cursor: 'pointer'}}>
+                        <Icon name="share" color="grey" />
+                        {"share"}
+                      </span>
+                    </div>
+                  </Grid.Row>
+                </Grid>
+              </Container>
+            </Grid.Column>
+          </Grid>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileHeader;
