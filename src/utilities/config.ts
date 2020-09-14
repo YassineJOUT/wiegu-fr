7b572083f9f23edf1321d00c89c15414a79a83b0
Yassine JOUT <yassinejout@gmail.com>
2020-09-14 12:24:32 +0200

    fix(Slider): arrows

--- src/utilities/config.ts
+++ src/utilities/config.ts
@@ -1,4 +1,4 @@
 const isDev = process.env.REACT_APP_ENV === "development";
 export const API_URL = isDev
-  ? "http://localhost:3010/"
-  : "http://localhost:3010/";
+  ? "http://weigu-api.yj-dev.com/"
+  : "http://weigu-api.yj-dev.com/";
